a7fae3bb47d43aedc8899bda06ae27fcc3b26b8e
louistychon <louis.tychon1@gmail.com>
2022-07-13 16:40:28 +0200

    buttons onhover on products page

--- src/components/products/productItem.js
+++ src/components/products/productItem.js
@@ -1,13 +1,17 @@
 import React, { useState} from 'react';
 import test from "../../assets/fashion-recent-products-06-1.jpg";
+import item4 from "../../assets/item-04.jpg";
+import item15 from "../../assets/item-15.jpg";
+import item16 from "../../assets/item-16.jpg";
 import "./productItem.css";
+import ProductCard from './productCard';
 
 
-const products = [
+export const data = [
     {id: 1, name: "lorem ipsum", price: 75.00, img: test, new: false, sale: true, newprice: 45.00, category: "Kids"}, 
-    {id: 2, name: "lorem ipsum", price: 75.00, img: test, new: false, sale: false, newprice: "", category: "Accessories"},
-    {id: 3, name: "lorem ipsum", price: 75.00, img: test, new: true, sale: false, newprice: "", category: "Accessories"}, 
-    {id: 4, name: "lorem ipsum", price: 75.00, img: test, new: true, sale: false, newprice: "", category: "Women"},
+    {id: 2, name: "lorem ipsum", price: 75.00, img: item4, new: false, sale: false, newprice: "", category: "Accessories"},
+    {id: 3, name: "lorem ipsum", price: 75.00, img: item15, new: true, sale: false, newprice: "", category: "Accessories"}, 
+    {id: 4, name: "lorem ipsum", price: 75.00, img: item16, new: true, sale: false, newprice: "", category: "Women"},
     {id: 5, name: "lorem ipsum", price: 75.00, img: test, new: false, sale: false, newprice: "", category: "Accessories"}, 
     {id: 6, name: "lorem ipsum", price: 75.00, img: test, new: false, sale: false, newprice: "", category: "Women"},
     {id: 7, name: "lorem ipsum", price: 75.00, img: test, new: false, sale: false, newprice: "", category: "Kids"}, 
@@ -18,32 +22,14 @@ const products = [
 export default function ProductItem (){
         return(
             <div>
-                <p className="number-products">Number of products : {products.length}</p>
-            <div className="product-gallery">
-            {products.map((product) => (
-                <>
-                <div>
-                <div key={product.id} style={{
-                    backgroundImage: "url(" + product.img + ")",backgroundPosition: 'center',
-                    backgroundSize: 'cover',
-                    backgroundRepeat: 'no-repeat', 
-                    height: "500px", maxWidth: "400px", 
-                    }}>
-                        {product.sale===true?<div className='sale-red-pill'>Sale</div>: ""}
-                        {product.new===true?<div className='new-green-pill'>New</div>: ""}
-                    </div>
-                    
-                <div>
-                    <p>{product.name}</p>
-                    <div className='product-prices-flex-container'>
-                        <p>{product.sale === true?<span className='onsale-price-line-through'>{product.price}$</span> : product.price + "$"}</p>
-                        <p>{product.newprice !== ""?<span className='newprice'>{product.newprice}$</span>: ""}</p>
-                    </div>
-                </div>
-                </div>
-                </>
+                <p className="number-products">Number of products : {data.length}</p>
+                <div className="product-gallery">
+                {data.map((product) => (
+                    <>
+                    <ProductCard id={product.id} name={product.name} price={product.price} img={product.img} new={product.new} sale={product.sale} newprice={product.newprice} category={product.category}/>
+                    </>
                 ))}
-            </div>
+                </div>
             </div>
         )
 }
