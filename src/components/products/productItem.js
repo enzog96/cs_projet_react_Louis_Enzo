c6fd1718b4c990cd956f99cb6e563e8e6bc6139d
louistychon <louis.tychon1@gmail.com>
2022-07-13 22:20:59 +0200

    inbasket property

--- src/components/products/productItem.js
+++ src/components/products/productItem.js
@@ -8,15 +8,15 @@ import ProductCard from './productCard';
 
 
 export const data = [
-    {id: 1, name: "lorem ipsum", price: 75.00, img: test, new: false, sale: true, newprice: 45.00, category: "Kids"}, 
-    {id: 2, name: "lorem ipsum", price: 75.00, img: item4, new: false, sale: false, newprice: "", category: "Accessories"},
-    {id: 3, name: "lorem ipsum", price: 75.00, img: item15, new: true, sale: false, newprice: "", category: "Accessories"}, 
-    {id: 4, name: "lorem ipsum", price: 75.00, img: item16, new: true, sale: false, newprice: "", category: "Men"},
-    {id: 5, name: "lorem ipsum", price: 75.00, img: test, new: false, sale: false, newprice: "", category: "Accessories"}, 
-    {id: 6, name: "lorem ipsum", price: 75.00, img: test, new: false, sale: false, newprice: "", category: "Women"},
-    {id: 7, name: "lorem ipsum", price: 75.00, img: test, new: false, sale: false, newprice: "", category: "Kids"}, 
-    {id: 8, name: "lorem ipsum", price: 75.00, img: test, new: false, sale: false, newprice: "", category: "Kids"},
-    {id: 9, name: "lorem ipsum", price: 75.00, img: test, new: true, sale: false, newprice: "", category: "Kids"},
+    {id: 1, name: "lorem ipsum", price: 75.00, img: test, new: false, sale: true, newprice: 45.00, category: "Kids", inbasket: false}, 
+    {id: 2, name: "lorem ipsum", price: 75.00, img: item4, new: false, sale: false, newprice: "", category: "Accessories", inbasket: false},
+    {id: 3, name: "lorem ipsum", price: 75.00, img: item15, new: true, sale: false, newprice: "", category: "Accessories", inbasket: false}, 
+    {id: 4, name: "lorem ipsum", price: 75.00, img: item16, new: true, sale: false, newprice: "", category: "Men", inbasket: false},
+    {id: 5, name: "lorem ipsum", price: 75.00, img: test, new: false, sale: false, newprice: "", category: "Accessories", inbasket: false}, 
+    {id: 6, name: "lorem ipsum", price: 75.00, img: test, new: false, sale: false, newprice: "", category: "Women", inbasket: false},
+    {id: 7, name: "lorem ipsum", price: 75.00, img: test, new: false, sale: false, newprice: "", category: "Kids", inbasket: false}, 
+    {id: 8, name: "lorem ipsum", price: 75.00, img: test, new: false, sale: false, newprice: "", category: "Kids", inbasket: false},
+    {id: 9, name: "lorem ipsum", price: 75.00, img: test, new: true, sale: false, newprice: "", category: "Kids", inbasket: false},
 ]
 
 export default function ProductItem (props){
